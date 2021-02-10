74a49f37b585e2fe4eb2eb9b59322510e52e061d
francisco-1998 <francmarinc@gmail.com>
2021-02-10 11:32:36 -0500

    Haciendo un todo basico

--- src/app/template-forms/dinamicos/dinamicos.component.ts
+++ src/app/template-forms/dinamicos/dinamicos.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface Persona {
+  nombre: string,
+  juegosFavoritos: Favoritos[]
+}
+
+interface Favoritos {
+  id: number
+  nombre: string
+}
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styleUrls: ['./dinamicos.component.css']
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
+  nuevoJuego: string = '';
+  @ViewChild('formularioDinamico') formularioDinamico!: NgForm
 
-  constructor() { }
+  get nombreInvalido() {
+    return this.formularioDinamico?.controls.nombrePersona?.invalid && this.formularioDinamico?.controls.nombrePersona?.touched;
+  }
 
-  ngOnInit(): void {
+  persona: Persona = {
+    nombre: '',
+    juegosFavoritos: []
+  }
+
+  guardar() {
+    console.log('Posteo');
+  }
+
+  eliminar(i: number) {
+    this.persona.juegosFavoritos.splice(i, 1)
+  }
+
+  addJuego() {
+    const nuevoFavorito: Favoritos = {
+      id: this.persona.juegosFavoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+    this.persona.juegosFavoritos.push({ ...nuevoFavorito });
+    this.nuevoJuego = '';
   }
 
 }
