b551f033272982ad9b8413ef4c5382e5d30abc48
francisco-1998 <francmarinc@gmail.com>
2021-02-09 21:40:12 -0500

    Reforzando los conceptos y practicas de los formularios de aproximacion por template

--- src/app/template-forms/basicos/basicos.component.ts
+++ src/app/template-forms/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
+  @ViewChild('miFormulario') miFormulario!:NgForm;
+
+  get nombreValido(){
+    return this.miFormulario?.controls.nombreProducto?.invalid && this.miFormulario?.controls.nombreProducto?.touched
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  guardar() {
+    console.log(this.miFormulario);
   }
 
 }
--- src/app/template-forms/template-forms.module.ts
+++ src/app/template-forms/template-forms.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { TemplateFormsRoutingModule } from './template-forms-routing.module';
 import { BasicosComponent } from './basicos/basicos.component';
@@ -16,7 +17,8 @@ import { SwitchesComponent } from './switches/switches.component';
   ],
   imports: [
     CommonModule,
-    TemplateFormsRoutingModule
+    TemplateFormsRoutingModule,
+    FormsModule
   ],
   exports: [
 
