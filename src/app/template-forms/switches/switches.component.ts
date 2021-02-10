91b319eb79084e0653f48982f6ddd3ffbd642646
francisco-1998 <francmarinc@gmail.com>
2021-02-10 12:15:09 -0500

    Fin de la practica de formularios por template

--- src/app/template-forms/switches/switches.component.ts
+++ src/app/template-forms/switches/switches.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
   styleUrls: ['./switches.component.css']
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  persona = {
+    genero: '',
+    notificaciones: true
   }
+
+  termCond = false;
 
 }
