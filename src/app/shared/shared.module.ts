755f31f4b4f9923e3332364f927b48a60177e51b
francisco-1998 <francmarinc@gmail.com>
2021-02-09 19:28:28 -0500

    SideMenu con las rutas listo

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SideMenuComponent } from './side-menu/side-menu.component';
+import { TemplateFormsModule } from '../template-forms/template-forms.module';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,7 +11,8 @@ import { SideMenuComponent } from './side-menu/side-menu.component';
     SideMenuComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
     SideMenuComponent
--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
+interface menuItem{
+  texto:string,
+  ruta:string
+}
+
+
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.css']
 })
-export class SideMenuComponent implements OnInit {
+export class SideMenuComponent {
 
-  constructor() { }
+  templateMenu: menuItem[]= [
+    {
+      texto: 'Básicos',
+      ruta: '/template-forms/basicos'
+    },
+    {
+      texto: 'Dinámicos',
+      ruta: '/template-forms/dinamicos'
+    },
+    {
+      texto: 'Switches',
+      ruta: '/template-forms/switches'
+    }
+  ]
 
-  ngOnInit(): void {
-  }
+  reactiveMenu: menuItem[]= [
+    {
+      texto: 'Básicos',
+      ruta: '/reactive-forms/basicos'
+    },
+    {
+      texto: 'Dinámicos',
+      ruta: '/reactive-forms/dinamicos'
+    },
+    {
+      texto: 'Switches',
+      ruta: '/reactive-forms/switches'
+    }
+  ]
 
 }
+
