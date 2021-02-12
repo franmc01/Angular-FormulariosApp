436cc59da117a7d6bc1afc40d6b36bca9bbef452
francisco-1998 <francmarinc@gmail.com>
2021-02-12 14:02:37 -0500

    Fin de lo reactivos dianmicos

--- src/app/reactive-forms/switches/switches.component.ts
+++ src/app/reactive-forms/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -7,9 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  persona = {
+    genero: 'F',
+    notificaciones: true
+  }
+
+  miformularioSwitch: FormGroup = this.fb.group({
+    genero: ['M', Validators.required],
+    notificaciones: [true, Validators.required],
+    condiciones: [false, Validators.requiredTrue]
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miformularioSwitch.reset({
+      ...this.persona,
+      condiciones: false
+    });
+
+    //RXJS
+
+    //Subscripcion a un campo en especifico
+    // this.miformularioSwitch.get('condiciones')?.valueChanges.subscribe(valordelcampo =>{
+    //   console.log('Valor del campo condiciones: ', valordelcampo);
+    // })
+
+    //Subscripcion a todo mi formulario
+    // this.miformularioSwitch.valueChanges.subscribe(({condiciones, ...demascampos})=>{
+    this.miformularioSwitch.valueChanges.subscribe(form => {
+      // delete form.condiciones;
+      const { genero, notificaciones } = form;
+      this.persona = { genero, notificaciones };
+      //this.persona = demascampos;
+      // this.persona = form
+      // console.log('Persona:', this.persona);
+    })
+  }
+
+
+  guardar() {
+    const formValue = { ...this.miformularioSwitch.value };
+    delete formValue.condiciones;
+
+    this.persona = formValue;
+
+    console.log(formValue);
   }
 
 }
