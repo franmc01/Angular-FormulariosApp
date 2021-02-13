56ac46addc0eef79f603235729ba73da299d2c91
francisco-1998 <francmarinc@gmail.com>
2021-02-13 16:21:20 -0500

    Agregando validaciones pattern

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { RegistroComponent } from './registro/registro.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/registro/registro.component.ts
+++ src/app/auth/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  // TODO: Temporal
+  nombreApellidoPattern: string = '([a-zA-z]+) ([a-zA-z]+)';
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+  formularioRegistro: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPattern)]],
+    correo: ['', [Validators.required, Validators.pattern(this.emailPattern)]]
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+
+  campoNoValido(campo:string){
+    return this.formularioRegistro.get(campo)?.invalid && this.formularioRegistro.get(campo)?.touched;
+  }
+
+  submitFormulario(){
+    if(this.formularioRegistro.invalid){
+      this.formularioRegistro.markAllAsTouched();
+    return;
+  }
+  }
 }
