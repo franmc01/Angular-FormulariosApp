d9dbdbbc58b61130b5d72992f99b8a4682a95b2b
francisco-1998 <francmarinc@gmail.com>
2021-02-15 19:05:35 -0500

    practicando la validacion de contraseña iguales de dos formas

--- src/app/auth/registro/registro.component.ts
+++ src/app/auth/registro/registro.component.ts
@@ -14,23 +14,35 @@ export class RegistroComponent implements OnInit {
   formularioRegistro: FormGroup = this.fb.group({
     nombre: ['Francisco Marin', [Validators.required, Validators.pattern(nombreApellidoPattern)]],
     correo: ['f@f.com', [Validators.required, Validators.pattern(emailPattern)]],
-    username: [ 'NoSisco', [Validators.required, this.vs.noPuedeSisco]]
+    username: ['NoSisco', [Validators.required, this.vs.noPuedeSisco]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', [Validators.required]]
+  },{
+    validators: [this.vs.camposIguales('password', 'confirmPassword')]
+    //Segunda forma de hacerlo
+    // validators: [this.vs.passwordIguales('password', 'confirmPassword')]
   });
 
-  constructor(private fb: FormBuilder, private vs:ValidatorService) { }
+  constructor(private fb: FormBuilder, private vs: ValidatorService) { }
+
+  // get pass2NoValido() {
+  //   const pass1 = this.formularioRegistro.get('password')?.value;
+  //   const pass2 = this.formularioRegistro.get('confirmPassword')?.value;
+  //   return (pass1 === pass2) ? false : true;
+  // }
 
   ngOnInit(): void {
   }
 
 
-  campoNoValido(campo:string){
+  campoNoValido(campo: string) {
     return this.formularioRegistro.get(campo)?.invalid && this.formularioRegistro.get(campo)?.touched;
   }
 
-  submitFormulario(){
-    if(this.formularioRegistro.invalid){
+  submitFormulario() {
+    if (this.formularioRegistro.invalid) {
       this.formularioRegistro.markAllAsTouched();
-    return;
-  }
+      return;
+    }
   }
 }
--- src/app/shared/validators/validator.service.ts
+++ src/app/shared/validators/validator.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormControl, ValidationErrors } from '@angular/forms';
+import { AbstractControl, FormControl, ValidationErrors, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -22,6 +22,35 @@ export class ValidatorService {
       }else{
         //En las validaciones si se hace return de un significa todo ok
         return null;
+      }
+    }
+
+
+    camposIguales(campo1:string, campo2:string){
+
+      return (formGroup:AbstractControl): ValidationErrors | null =>{
+        const pass1 = formGroup.get(campo1)?.value;
+        const pass2 = formGroup.get(campo2)?.value;
+
+        if(pass1 !== pass2){
+          formGroup.get(campo2)?.setErrors({camposNoIguales:true})
+          return { camposNoIguales: true }
+        }
+        formGroup.get(campo2)?.setErrors(null)
+        return null;
+      }
+    }
+
+    passwordIguales(pass1Name:string, pass2Name:string){
+      return (miFormulario:FormGroup)=>{
+        const pass1Control=miFormulario.controls[pass1Name];
+        const pass2Control=miFormulario.controls[pass2Name];
+
+        if(pass1Control===pass2Control){
+           pass2Control.setErrors(null);
+        }else{
+          pass2Control.setErrors({camposNoSonIguales:true})
+        }
       }
     }
 }
